36e1b41af24ade2956dac5ff0e1ad3e1ecf0d624
Workplace <tayyabahmed7561@gmail.com>
2025-01-13 05:49:22 -0800

    work

---
+++ app/Pages/Featured/data/page.tsx
@@ -0,0 +1,35 @@
+export const ProductsData = [
+    {
+        id:"1",    
+        name:"Cantilever chair",
+        code:"Code - Y523201",
+        price:"$42.00",
+        image:"/Products/chair.svg",
+        desc:"The Cantilever Chair is a perfect blend of modern design and comfort. With its sleek, minimalistic look and ergonomic structure, it provides a stylish yet comfortable seating solution for both home and office spaces. The chair features a unique cantilever frame that allows for flexible seating, ensuring a supportive yet relaxed experience. Its padded seat and backrest provide comfort for extended periods, making it ideal for workspaces, living rooms, or even meeting areas. The clean lines and contemporary design of the chair make it an excellent addition to any modern interior, offering both functionality and aesthetic appeal.Whether you're looking to furnish your office or add a sophisticated touch to your home decor, the Cantilever Chair is designed to complement any modern setting. Durable, sturdy, and easy to maintain, this chair is built to withstand daily use while maintaining its stylish appearance."
+    },
+    {
+        id:"2",    
+        name:"Cantilever chair",
+        code:"Code - Y523201",
+        price:"$42.00",
+        image:"/Products/chair2.svg",
+         desc:"The Cantilever Chair is a perfect blend of modern design and comfort. With its sleek, minimalistic look and ergonomic structure, it provides a stylish yet comfortable seating solution for both home and office spaces. The chair features a unique cantilever frame that allows for flexible seating, ensuring a supportive yet relaxed experience. Its padded seat and backrest provide comfort for extended periods, making it ideal for workspaces, living rooms, or even meeting areas. The clean lines and contemporary design of the chair make it an excellent addition to any modern interior, offering both functionality and aesthetic appeal.Whether you're looking to furnish your office or add a sophisticated touch to your home decor, the Cantilever Chair is designed to complement any modern setting. Durable, sturdy, and easy to maintain, this chair is built to withstand daily use while maintaining its stylish appearance."
+    },
+    {
+        id:"3",    
+        name:"Cantilever chair",
+        code:"Code - Y523201",
+        price:"$42.00",
+        image:"/Products/chair4.svg",
+         desc:"The Cantilever Chair is a perfect blend of modern design and comfort. With its sleek, minimalistic look and ergonomic structure, it provides a stylish yet comfortable seating solution for both home and office spaces. The chair features a unique cantilever frame that allows for flexible seating, ensuring a supportive yet relaxed experience. Its padded seat and backrest provide comfort for extended periods, making it ideal for workspaces, living rooms, or even meeting areas. The clean lines and contemporary design of the chair make it an excellent addition to any modern interior, offering both functionality and aesthetic appeal.Whether you're looking to furnish your office or add a sophisticated touch to your home decor, the Cantilever Chair is designed to complement any modern setting. Durable, sturdy, and easy to maintain, this chair is built to withstand daily use while maintaining its stylish appearance."
+    },
+    {
+        id:"4",    
+        name:"Cantilever chair",
+        code:"Code - Y523201",
+        price:"$42.00",
+        image:"/Products/chair5.svg",
+        desc:"The Cantilever Chair is a perfect blend of modern design and comfort. With its sleek, minimalistic look and ergonomic structure, it provides a stylish yet comfortable seating solution for both home and office spaces. The chair features a unique cantilever frame that allows for flexible seating, ensuring a supportive yet relaxed experience. Its padded seat and backrest provide comfort for extended periods, making it ideal for workspaces, living rooms, or even meeting areas. The clean lines and contemporary design of the chair make it an excellent addition to any modern interior, offering both functionality and aesthetic appeal.Whether you're looking to furnish your office or add a sophisticated touch to your home decor, the Cantilever Chair is designed to complement any modern setting. Durable, sturdy, and easy to maintain, this chair is built to withstand daily use while maintaining its stylish appearance." 
+    },
+ 
+]
