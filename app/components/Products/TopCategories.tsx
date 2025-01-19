37abf635472a73b7a4ad4acf6b6a5d465b32ac05
Workplace <tayyabahmed7561@gmail.com>
2025-01-19 11:37:44 -0800

    final

--- app/components/Products/TopCategories.tsx
+++ app/components/Products/TopCategories.tsx
@@ -3,7 +3,17 @@ import { Josefin_Sans } from 'next/font/google'
 import { client } from '@/sanity/lib/client';
 import Image from 'next/image';
 
-const getData = async () => {
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  quantity: number;
+  price: number;
+  image_url: string;
+  rating?: number; // Optional field
+}
+
+const getData = async ():Promise<Product[]> => {
   const response = await client.fetch(`
     [
       *[_type == "product" && category == "Chair"][13],
@@ -34,7 +44,7 @@ const TopCategories = async () => {
     <section className='flex justify-center items-center flex-col container mx-auto'>
         <div><h1 className={`${Josefin.className} font-bold text-4xl p-9 text-[#1A0B5B]`}>Top Categories</h1></div>
         <div className='flex items-center justify-center h-auto w-auto gap-[39px] flex-wrap'>
-            {product.map((chairs:any,index:number)=>(
+            {product.map((chairs:Product,index:number)=>(
                 <div key={index} className='w-[269px] h-[345px]  flex flex-col justify-between items-center'>
                 <div className='h-[269px] w-[269px] rounded-full bg-[#F6F7FB] flex items-center justify-center'>
                     <Image className='h-[178px] w-[178px]' src={chairs.image_url} alt="" height={100} width={100}/>
