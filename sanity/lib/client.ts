ea6f89dd0ba5cd1661f206425c9bd57570ec5693
Workplace <tayyabahmed7561@gmail.com>
2025-01-19 12:01:03 -0800

    changes

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion} from '../env'
+import {apiVersion,dataset,projectId} from '../env'
 
 export const client = createClient({
-  projectId:"wj5gd26k",
-  dataset:"production",
+  projectId,
+  dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
